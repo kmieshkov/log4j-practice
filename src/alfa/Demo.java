bc583d661f9c8946b6fc0d0e6998070a34295eb8
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2022-04-15 14:55:41 -0400

    first commit

---
+++ src/alfa/Demo.java
@@ -0,0 +1,13 @@
+package alfa;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Demo {
+	private static Logger log = LogManager.getLogger(Demo.class.getName());
+
+	public static void main(String[] args) {
+		log.atError();
+	}
+}
